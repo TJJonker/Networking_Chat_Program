eb3a6a8e3cb6a689054e6422c88134ee4e5597fd
Tom Jonker <tjj.jonker@gmail.com>
2023-10-16 23:35:17 -0400

    Removed some debug leftovers

--- Chat_Server/src/Server/Commands/Commands.cpp
+++ Chat_Server/src/Server/Commands/Commands.cpp
@@ -28,24 +28,17 @@ bool Commands::AddClientToRooms(std::shared_ptr<Client> client, std::string room
 	TwoNet::Buffer buffer;
 	std::string response;
 
-	TWONET_LOG_TRACE("Check inside AddClientToRooms method.");
 	Room* room = roomManager.GetRoom(roomName);
 	result = room->AddClient(client);
 	if (!result) {
 		response = TwoNet::Utils::ResponseToString(TwoNet::Utils::Response::FAILED);
-		TWONET_LOG_INFO("response: {0}", response);
 		buffer.SerializeData(response.c_str(), response.length());
 		server.SendData(client->Socket, buffer);
 		return false;
 	}
 
 	response = TwoNet::Utils::ResponseToString(TwoNet::Utils::Response::SUCCESS);
-	TWONET_LOG_INFO("response: {0}", response.c_str());
-	TWONET_LOG_INFO("response length: {0}", response.length());
 	TwoNet::TwoProt::SerializeData(buffer, response.c_str(), response.length());
-	TWONET_LOG_INFO("response buffer: {0}", buffer.GetBufferData());
-	TWONET_LOG_INFO("response buffer length: {0}", buffer.GetBufferSize());
-	TWONET_LOG_INFO("response buffer write: {0}", buffer.GetSize());
 	server.SendData(client->Socket, buffer);
 	return true;
 }
